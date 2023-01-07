9b963f1e02358f37a8579a901969ecb1877d23c8
Vaishnav <vaishnav7486@gmail.com>
2023-01-07 13:33:13 +0530

    minor changes in second page UI

--- lib/screens/home screen/location_selected_screen.dart
+++ lib/screens/home screen/location_selected_screen.dart
@@ -18,34 +18,188 @@ class LocationSelectedScreen extends StatelessWidget {
           children: [
             SizedBox(height: default_padding / 1.5),
             ImageCardL(),
-            Row(
-              children: [
-                Column(
-                  children: [
-                    Text(
-                      "Denver Mountains",
-                      style: TextStyle(
-                        fontSize: default_fontsize * 1.5,
-                      ),
-                    ),
-                    RichText(
-                        text: TextSpan(children: [
-                      WidgetSpan(
-                          child: Icon(
-                        Icons.location_on_outlined,
-                        size: default_fontsize,
-                        color: textRed,
-                      )),
-                      TextSpan(
-                          text: "Denver, Colorado",
-                          style: TextStyle(color: textRed))
-                    ]))
-                  ],
-                )
-              ],
+            NameLocAndRatingProductView(),
+            DetailsCommentsReviews(),
+            DetailsTextContent(),
+            Container(
+              height: 80,
+              width: MediaQuery.of(context).size.width,
+              color: grey1,
             )
           ],
         )),
+      ),
+    );
+  }
+}
+
+class DetailsTextContent extends StatelessWidget {
+  const DetailsTextContent({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+          horizontal: default_padding, vertical: default_padding * 1.5),
+      child: Text(
+        """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
+        Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.""",
+        style: TextStyle(
+            fontSize: default_fontsize,
+            color: textGrey,
+            fontWeight: FontWeight.w300),
+      ),
+    );
+  }
+}
+
+class DetailsCommentsReviews extends StatelessWidget {
+  const DetailsCommentsReviews({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+          horizontal: default_padding, vertical: default_padding / 2),
+      child: Row(
+        children: [
+          textbuttonwithpaddingSelected(
+            text: "Details",
+          ),
+          textbuttonwithpadding(
+            text: "Comments",
+          ),
+          textbuttonwithpadding(
+            text: "Reviews",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class textbuttonwithpaddingSelected extends StatelessWidget {
+  const textbuttonwithpaddingSelected({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: default_padding),
+      padding: EdgeInsets.symmetric(
+          vertical: default_padding / 1.8, horizontal: default_padding / 1.2),
+      decoration: BoxDecoration(
+          color: grey2,
+          borderRadius: BorderRadius.circular(default_BRadius / 2)),
+      child: Text(
+        text,
+        style:
+            TextStyle(fontSize: default_fontsize, fontWeight: FontWeight.w600),
+      ),
+    );
+  }
+}
+
+class textbuttonwithpadding extends StatelessWidget {
+  const textbuttonwithpadding({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        right: default_padding * 1.5,
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+            color: textGrey,
+            fontSize: default_fontsize,
+            fontWeight: FontWeight.w300),
+      ),
+    );
+  }
+}
+
+class NameLocAndRatingProductView extends StatelessWidget {
+  const NameLocAndRatingProductView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+          horizontal: default_padding, vertical: default_padding * 1.2),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Denver Mountains",
+                style: TextStyle(
+                    fontSize: default_fontsize * 1.7,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black87),
+              ),
+              SizedBox(height: default_padding / 3),
+              RichText(
+                  text: TextSpan(children: [
+                WidgetSpan(
+                    child: Icon(
+                  Icons.location_on_outlined,
+                  size: default_fontsize,
+                  color: textRed,
+                )),
+                TextSpan(
+                    text: "Denver, Colorado",
+                    style: TextStyle(
+                        color: textRed, fontSize: default_fontsize / 1.2))
+              ]))
+            ],
+          ),
+          Row(
+            children: [
+              Icon(
+                Icons.star,
+                color: Colors.amber.shade500,
+              ),
+              SizedBox(width: default_padding / 2),
+              Column(
+                children: [
+                  Text(
+                    '4.9/5',
+                    style: TextStyle(
+                        fontSize: default_fontsize * 1.2,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.amber.shade500),
+                  ),
+                  Text(
+                    "(17,500)",
+                    style: TextStyle(
+                        fontSize: default_fontsize / 1.5,
+                        // fontWeight: FontWeight.w600,
+                        color: textGrey2),
+                  ),
+                ],
+              )
+            ],
+          )
+        ],
       ),
     );
   }
